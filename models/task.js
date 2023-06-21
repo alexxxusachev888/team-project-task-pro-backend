c484752a98dd9f31d02021221f33df179fb52666
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2023-06-21 16:04:21 +0300

    added task model

---
+++ models/task.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleMongooseError, errorMessages } = require("../helpers");
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Task`s title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Task`s description is required"],
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High", "Without"],
+      required: [true, "Task`s priority is required"],
+      //   default: "Without",
+    },
+    deadline: {
+      type: Date,
+      required: [true, "Task`s deadline is required"],
+      min: Date.now(),
+      //   default: Date.now(),
+    },
+    board: {
+      type: Schema.Types.ObjectId,
+      ref: "board",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Task = model("task", taskSchema);
+taskSchema.post("save", handleMongooseError);
+
+const taskSchemaJoi = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  priority: Joi.string().valid("Low", "Medium", "High", "Without").required(),
+  deadline: Joi.date().min("now").required(),
+  board: Joi.string().required(),
+}).options({ abortEarly: false });
+
+module.exports = { Task, taskSchemaJoi };
