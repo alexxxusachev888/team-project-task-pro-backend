69618acd99131281491049723464565302009d61
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-24 22:05:17 +0300

    Update test boards routs

--- controllers/boardsTest.js
+++ controllers/boardsTest.js
@@ -1,43 +1,26 @@
 const { ctrlWrapper, handleHttpError } = require('../helpers');
-const { populate } = require('dotenv');
 
 const { BoardTest, ColumnTest, TaskTest } = require('../models/boardTest');
 
 const getAllBoardTest = async (req, res) => {
-  //   const { _id: owner } = req.user;
   let boards = await BoardTest.find();
   boards = boards || [];
   res.status(200).json(boards);
 };
 
 const getBoardByIdTest1 = async (req, res) => {
-  //   const { _id: owner } = req.user;
-  const { id: boards } = req.params;
-
-  let columns = await ColumnTest.find({ boards });
-  let tasks = await TaskTest.find({ boards });
-
-  columns = columns || [];
-  tasks = tasks || [];
-
-  res.status(200).json({ columns, tasks });
-};
-
-const getBoardByIdTest2 = async (req, res) => {
-  // const { _id: owner } = req.user;
-  // const board = await Board.findOne({ owner }).populate({
   const { id } = req.params;
-  const board = await BoardTest.findOne({ id }).populate({
-    path: 'columns',
-    populate: {
-      path: 'tasks',
-    },
-  });
-  res.status(200).json(board);
+  const board = await BoardTest.findOne({ id });
+  if (!board) throw handleHttpError(404, `Board with id ${id} not found`);
+
+  const boards = id;
+  const columns = (await ColumnTest.find({ boards })) || [];
+  const tasks = (await TaskTest.find({ boards })) || [];
+
+  res.status(200).json({ ...board._doc, columns, tasks });
 };
 
 module.exports = {
   getAllBoardTest: ctrlWrapper(getAllBoardTest),
   getBoardByIdTest1: ctrlWrapper(getBoardByIdTest1),
-  getBoardByIdTest2: ctrlWrapper(getBoardByIdTest2),
 };
--- routes/boardTest.js
+++ routes/boardTest.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const {
   getAllBoardTest,
   getBoardByIdTest1,
-  getBoardByIdTest2,
 } = require('../controllers/boardsTest');
 
 router.get('/', getAllBoardTest);
 router.get('/test1/:id', getBoardByIdTest1);
-router.get('/test2/:id', getBoardByIdTest2);
 
 module.exports = router;
